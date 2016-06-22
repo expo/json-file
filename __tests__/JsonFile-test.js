648acc4930c05dcf28c475d4265160400dbde78f
Jesse Ruder <jesse@exp.host>
2016-06-22 11:10:15 -0700

    Fix tests

--- __tests__/JsonFile-test.js
+++ __tests__/JsonFile-test.js
@@ -13,22 +13,17 @@ describe('JsonFile', () => {
     expect(JsonFile.writeAsync).toBeDefined();
   });
 
-  // NOTE(brentvatne):
-  // the following tests break randomly, I believe due to instapromise,
-  // because I change the implementation from fs.promise.readFile to
-  // fs.readFile.promise
+  pit(`reads JSON from a file`, () => {
+    let file = new JsonFile('./package.json');
+    return file.readAsync().then(object => {
+      expect(object.version).toBeDefined();
+    });
+  });
 
-  // pit(`reads JSON from a file`, () => {
-  //   let file = new JsonFile('./package.json');
-  //   return file.readAsync().then(object => {
-  //     expect(object.version).toBeDefined();
-  //   });
-  // });
-
-  // pit(`reads JSON5 from a file`, () => {
-  //   let file = new JsonFile('./test-json5.json', {json5: true});
-  //   return file.readAsync().then(object => {
-  //     expect(object.itParsedProperly).toBe(42);
-  //   });
-  // });
+  pit(`reads JSON5 from a file`, () => {
+    let file = new JsonFile('./test-json5.json', {json5: true});
+    return file.readAsync().then(object => {
+      expect(object.itParsedProperly).toBe(42);
+    });
+  });
 });
