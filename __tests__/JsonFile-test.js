7980197c3e1dcad26cf60274e3ca9b1928bc34ea
wilzh40 <wilzh40@gmail.com>
2017-07-07 11:57:24 -0700

    Added more race condition tests

--- __tests__/JsonFile-test.js
+++ __tests__/JsonFile-test.js
@@ -1,13 +1,18 @@
 'use strict';
 
-let JsonFile = require('../JsonFile');
-let fs = require('mz/fs');
-let mock = require('mock-fs');
-let lockFile = require('lockfile');
+const JsonFile = require('../JsonFile');
+const fs = require('mz/fs');
+const path = require('path')
+const mock = require('mock-fs');
+const lockFile = require('lockfile');
+const cp = require('child_process');
+const _ = require('lodash');
 
 const j = JSON.stringify;
 
-describe('JsonFile', () => {
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 2 * 60 * 1000;
+
+describe('JsonFile Basic Tests', () => {
   it(`is a class`, () => {
     let file = new JsonFile(path.join(__dirname, '../package.json'));
     expect(file instanceof JsonFile).toBe(true);
@@ -70,7 +75,6 @@ describe('JsonFile mockjs basic integration test', () => {
     await expect(fs.existsSync('./write-test.json')).toBe(true);
     let file = new JsonFile('./write-test.json', { json5: true });
     await expect(file.setAsync('x', 2)).resolves;
-    // await setAsync('y', 30)
     await expect(file.readAsync()).resolves.toEqual({ x: 2 });
   });
 
@@ -78,7 +82,6 @@ describe('JsonFile mockjs basic integration test', () => {
     await expect(fs.existsSync('./write-test.json')).toBe(true);
     let file = new JsonFile('./write-test.json', { json5: true });
     await expect(file.setAsync('y', 3)).resolves;
-    // await setAsync('y', 30)
     await expect(file.readAsync()).resolves.toEqual({ x: 2, y: 3 });
   });
 
@@ -86,7 +89,6 @@ describe('JsonFile mockjs basic integration test', () => {
     await expect(fs.existsSync('./write-test.json')).toBe(true);
     let file = new JsonFile('./write-test.json', { json5: true });
     await expect(file.deleteKeyAsync('y')).resolves;
-    // await setAsync('y', 30)
     await expect(file.readAsync()).resolves.toEqual({ x: 2 });
   });
 
@@ -94,13 +96,11 @@ describe('JsonFile mockjs basic integration test', () => {
     await expect(fs.existsSync('./write-test.json')).toBe(true);
     let file = new JsonFile('./write-test.json', { json5: true });
     await expect(file.deleteKeyAsync('x')).resolves;
-    // await setAsync('y', 30)
     await expect(file.readAsync()).resolves.toEqual({});
   });
-
 });
 
-describe('JsonFile mockjs race condition tegration test', () => {
+describe('JsonFile mockjs race condition integration test', () => {
   beforeAll(() => {
     mock();
   });
@@ -109,13 +109,48 @@ describe('JsonFile mockjs race condition tegration test', () => {
     mock.restore();
   });
 
+  // The following test is not possible beacuse child processes do not inherit a mocked file system
+  xit(
+    'Multiple updates to the same file from different processes are atomic',
+    async () => {
+      let file = new JsonFile('atomic-test.json', { json5: true });
+      let baseObj = {};
+      for (var i = 0; i < 20; i++) {
+        const k = i.toString();
+        const v = i.toString();
+        baseObj = _.extend(baseObj, { [k]: v });
+        cp.fork('./worker-test.js', ['./atomic-test.json', k, v]);
+      }
+      // The following worker does a setAsync
+      //cp.fork('./JsonFileWorker', [filename, key, value])
+      const json = await file.readAsync();
+      console.log(json);
+      expect(json).toEqual(baseObj);
+    }
+  );
+
+  // This fails when i is high, around 200. However, no realistic use case would have the user
+  // constantly update a file that often
+  it('Multiple updates to the same file have no race conditions', async () => {
+    let file = new JsonFile('./atomic-test.json', { json5: true });
+    for (var i = 0; i < 50; i++) {
+      await file.writeAsync({});
+      let baseObj = {};
+      for (var i = 0; i < 20; i++) {
+        baseObj = _.extend(baseObj, { [i]: i });
+        file.setAsync(i, i);
+      }
+      const json = await file.readAsync();
+      expect(json).toEqual(baseObj);
+    }
+  });
+
   it('Continuous updating!', async () => {
     let file = new JsonFile('./write-test.json', { json5: true });
-    await file.writeAsync({i: 0});
-    for(var i = 0; i < 20; i++) {
-      file.writeAsync({i})
-      expect(file.readAsync()).resolves.toEqual({i})
+    await file.writeAsync({ i: 0 });
+    for (var i = 0; i < 20; i++) {
+      file.writeAsync({ i });
+      await expect(file.readAsync()).resolves.toEqual({ i });
     }
-  })
-})
-
+  });
+});
