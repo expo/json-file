e1ab59b44a367d325457b4dedeb7de67ffbfac81
wilzh40 <wilzh40@gmail.com>
2017-07-06 18:36:21 -0700

    Wrote a race condition test

--- __tests__/JsonFile-test.js
+++ __tests__/JsonFile-test.js
@@ -3,8 +3,7 @@
 let JsonFile = require('../JsonFile');
 let fs = require('mz/fs');
 let mock = require('mock-fs');
-let lockFile = require('lockfile')
-
+let lockFile = require('lockfile');
 
 const j = JSON.stringify;
 
@@ -42,7 +41,7 @@ describe('JsonFile', () => {
 });
 
 let obj1 = { x: 1 };
-describe('JsonFile mockjs tests', () => {
+describe('JsonFile mockjs basic integration test', () => {
   beforeAll(() => {
     mock();
   });
@@ -62,22 +61,61 @@ describe('JsonFile mockjs tests', () => {
   it(`rewrite async`, async () => {
     expect(fs.existsSync('./write-test.json')).toBe(true);
     let file = new JsonFile('./write-test.json', { json5: true });
-    expect(file.rewriteAsync()).resolves
-    expect(lockFile.checkSync('state.json.lock')).toBe(true)
-    expect(lockFile.checkSync('state.json.lock')).toBe(true)
-    expect(lockFile.checkSync('state.json.lock')).toBe(true)
-    expect(lockFile.checkSync('state.json.lock')).toBe(true)
-    expect(lockFile.checkSync('state.json.lock')).toBe(true)
+    await expect(file.rewriteAsync()).resolves;
     expect(fs.existsSync('./write-test.json')).toBe(true);
     await expect(file.readAsync()).resolves.toEqual(obj1);
   });
 
-
-  it(`changes a key in that file`, async () => {
+  it(`changes an existing key in that file`, async () => {
     await expect(fs.existsSync('./write-test.json')).toBe(true);
     let file = new JsonFile('./write-test.json', { json5: true });
-    await expect(file.setAsync('x', 1)).resolves;
+    await expect(file.setAsync('x', 2)).resolves;
     // await setAsync('y', 30)
     await expect(file.readAsync()).resolves.toEqual({ x: 2 });
   });
+
+  it(`adds a new key to the file`, async () => {
+    await expect(fs.existsSync('./write-test.json')).toBe(true);
+    let file = new JsonFile('./write-test.json', { json5: true });
+    await expect(file.setAsync('y', 3)).resolves;
+    // await setAsync('y', 30)
+    await expect(file.readAsync()).resolves.toEqual({ x: 2, y: 3 });
+  });
+
+  it(`deletes that same new key from the file`, async () => {
+    await expect(fs.existsSync('./write-test.json')).toBe(true);
+    let file = new JsonFile('./write-test.json', { json5: true });
+    await expect(file.deleteKeyAsync('y')).resolves;
+    // await setAsync('y', 30)
+    await expect(file.readAsync()).resolves.toEqual({ x: 2 });
+  });
+
+  it(`deletes another key from the file`, async () => {
+    await expect(fs.existsSync('./write-test.json')).toBe(true);
+    let file = new JsonFile('./write-test.json', { json5: true });
+    await expect(file.deleteKeyAsync('x')).resolves;
+    // await setAsync('y', 30)
+    await expect(file.readAsync()).resolves.toEqual({});
+  });
+
 });
+
+describe('JsonFile mockjs race condition tegration test', () => {
+  beforeAll(() => {
+    mock();
+  });
+
+  afterAll(() => {
+    mock.restore();
+  });
+
+  it('Continuous updating!', async () => {
+    let file = new JsonFile('./write-test.json', { json5: true });
+    await file.writeAsync({i: 0});
+    for(var i = 0; i < 20; i++) {
+      file.writeAsync({i})
+      expect(file.readAsync()).resolves.toEqual({i})
+    }
+  })
+})
+
